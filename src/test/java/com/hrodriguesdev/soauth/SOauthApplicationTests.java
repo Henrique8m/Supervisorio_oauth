ddc98d0c145d3faf2bec185f5f1efc035a05fce3
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-07-20 14:45:02 -0300

    Create project

---
+++ src/test/java/com/hrodriguesdev/soauth/SOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrodriguesdev.soauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
