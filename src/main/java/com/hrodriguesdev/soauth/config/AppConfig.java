a9617783481481499aa17fd64124e39c3fcd7b09
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-07-29 13:13:55 -0300

    Config git updatede

--- src/main/java/com/hrodriguesdev/soauth/config/AppConfig.java
+++ src/main/java/com/hrodriguesdev/soauth/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.hrodriguesdev.soauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +10,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+	
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -18,7 +22,7 @@ public class AppConfig {
 	@Bean
 	public JwtAccessTokenConverter jwtAccessTokenConverter() {
 		JwtAccessTokenConverter token = new JwtAccessTokenConverter();
-		token.setSigningKey("MY-SECRET-KEY");
+		token.setSigningKey(jwtSecret);
 		return token;
 	}
 	
--- src/main/java/com/hrodriguesdev/soauth/config/AuthorizationServerConfig.java
+++ src/main/java/com/hrodriguesdev/soauth/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.hrodriguesdev.soauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,6 +31,12 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Value("${oauth.client.name}")
+	private String oauthClientName;
+	
+	@Value("${oauth.client.secret}")
+	private String oauthClientSecret;
+	
 	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -40,8 +47,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
-		.withClient("myappname123")
-		.secret(passwordEncoder.encode("myappsecret123"))
+		.withClient(oauthClientName)
+		.secret(passwordEncoder.encode(oauthClientSecret))
 		.scopes("read", "write")
 		.authorizedGrantTypes("password")
 		.accessTokenValiditySeconds(1800);
